5e527f907ddc1f64f18b014c8d3903ef6a98558b
Bartosz Podemski <bartekpodemski@gmail.com>
2024-09-14 13:52:38 +0200

    delete unused imports

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -2,7 +2,7 @@
 import { Webhook } from "svix";
 import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
-import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { createUser } from "@/lib/actions/user.action";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
