99c56f4e696477ea8955a39c97a68a0fac601617
MichalWolanin <michalwolanin02@gmail.com>
2024-09-14 13:36:53 +0200

    Add form with buttons and inputs

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,10 +1,13 @@
-import { DeleteButton } from "@/components/ui/DeleteButton";
+import { FormComponent } from "../../../components/ui/form";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="text-xl m-auto text-center mt-8">Pomocnik zakupowy AI</h1>
-      <DeleteButton />
+      <h1 className="text-5xl font-semibold m-auto text-center mt-12 text-slate-600">
+        Pomocnik zakupowy AI
+      </h1>
+      <h2 className="text-xl font-semibold m-auto text-center mt-12 text-slate-400">Hej! Oto Twój pomocnik zakupowy, wprowadź nazwę pożądanego produktu i ciesz się najtańszą i najlepiej ocenianą ofertą na rynku!</h2>
+      <FormComponent/>
     </div>
   );
 }
