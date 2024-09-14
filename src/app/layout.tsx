1777303a7f4c4d1c753b0d0eeab3139986510b6e
Bartosz Podemski <bartekpodemski@gmail.com>
2024-09-14 10:01:44 +0200

    fix unused-vars error form eslint

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import { Header } from "@/components/ui/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
--- src/lib/actions/user.action.ts
+++ src/lib/actions/user.action.ts
@@ -6,8 +6,14 @@ import {
   DeleteUserParams,
 } from "./shared.types";
 
-export async function createUser(params: CreateUserParams) {}
+export async function createUser(params: CreateUserParams) {
+  const {} = params;
+}
 
-export async function updateUser(params: UpdateUserParams) {}
+export async function updateUser(params: UpdateUserParams) {
+  const {} = params;
+}
 
-export async function deleteUser(params: DeleteUserParams) {}
+export async function deleteUser(params: DeleteUserParams) {
+  const {} = params;
+}
