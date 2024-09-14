edc84a9a8a3cf5cc009e03a617c96369a6309d85
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-14 12:24:36 +0200

    add user interfaces for crud params

--- src/types/shared.types.d.ts
+++ src/types/shared.types.d.ts
@@ -1,11 +1,13 @@
 export interface CreateUserParams {
-
+    clerkUserId: string;
+    username?: string;
 }
 
 export interface UpdateUserParams {
-
+    clerkUserId: string;
+    username?: string;
 }
 
 export interface DeleteUserParams {
-    
+    clerkUserId: string;
 }
