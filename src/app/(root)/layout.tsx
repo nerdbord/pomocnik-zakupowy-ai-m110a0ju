2a9caa55f052d310af59955f31ca24b5050130d9
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-15 14:28:33 +0200

    feat: create footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/ui/Header";
 import React from "react";
 
@@ -6,6 +7,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
     <main>
       <Header />
       {children}
+      <Footer />
     </main>
   );
 };
---
+++ src/components/Footer.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import { Linkedin } from "lucide-react";
+
+export const Footer = () => {
+  const linksToProfiles = [
+    { href: "https://www.linkedin.com/in/michal-wolanin/", label: "Michał" },
+    { href: "https://www.linkedin.com/in/bartosz-podemski-b58a8a240/", label: "Bartek" },
+    {
+      href: "https://www.github.com/transcendence12",
+      label: "Gosia",
+    },
+  ];
+  return (
+    <footer className="mx-auto w-full flex-col items-center gap-5 bg-slate-100 p-4 mt-8 border-t">
+      <div className="mx-auto flex-col justify-center items-center text-sm max-w-[600px]">
+        <span className="flex justify-center items-center py-2 px-4">
+          &copy; {new Date().getFullYear()} - All rights reserved.
+        </span>
+        <ul className="flex justify-center items-center gap-8 py-2 px-8">
+          {linksToProfiles.map((link, index) => (
+            <li key={index} className="flex items-center gap-2">
+              <span>{link.label}</span>
+              <Link href={link.href} className="mr-1 hover:underline">
+                <Linkedin size={24} fill="blue" strokeWidth="1" />
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
+};
