459e314ef0b37a9861e5bbf4105e11bff0cedf07
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-15 17:59:32 +0200

    feat/form change button add to favorite

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { FormComponent } from "../../../components/ui/form";
+import { FormComponent } from "../../../components/form";
 
 export default function Home() {
   return (
@@ -6,7 +6,7 @@ export default function Home() {
       <h1 className="text-5xl font-semibold m-auto text-center mt-12 text-slate-600">
         Pomocnik zakupowy AI
       </h1>
-      <h2 className=" max-w-[800px] text-xl font-semibold m-auto text-center mt-12 text-slate-400">
+      <h2 className="max-w-[800px] text-xl font-semibold m-auto text-center mt-12 text-slate-400">
         Hej! Oto Twój pomocnik zakupowy, wprowadź nazwę pożądanego produktu i
         ciesz się najtańszą i najlepiej ocenianą ofertą na rynku!
       </h2>
--- src/components/FavoriteButton.tsx
+++ src/components/FavoriteButton.tsx
@@ -4,6 +4,7 @@ import { useEffect, useState, useTransition } from "react";
 import { Button } from "./ui/button";
 import { addFavorite, isFavorite } from "@/lib/actions/favorites.action";
 import { useToast } from "@/hooks/use-toast";
+import { Star, Check, Loader2 } from "lucide-react";
 
 interface FavoriteButtonProps {
   url: string;
@@ -31,7 +32,7 @@ export const FavoriteButton: React.FC<FavoriteButtonProps> = ({ url }) => {
         setIsAdded(true); // Ustaw jako dodany
 
         toast({
-          description: "Link został dodany do ulubionych.",
+          description: "Link został dodany do listy ulubionych.",
         });
       }
     });
@@ -39,7 +40,20 @@ export const FavoriteButton: React.FC<FavoriteButtonProps> = ({ url }) => {
   return (
     <div>
       <Button onClick={handleAddToFavorite} disabled={isPending || isAdded}>
-        {isPending ? "Dodawanie" : isAdded ? "Dodano" : "Dodaj do ulubionych"}
+        {/* {isPending ? "Zapisywanie" : isAdded ? "Zapisano" : "Zapisz w ulubionych"} */}
+        {isPending ? (
+          <>
+            <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Zapisywanie
+          </>
+        ) : isAdded ? (
+          <>
+            <Check className="mr-2 h-4 w-4" /> Zapisano
+          </>
+        ) : (
+          <>
+            <Star className="mr-2 h-4 w-4" /> Zapisz
+          </>
+        )}
       </Button>
     </div>
   );
