e08282527ba8791c5037becf761ed73da96b45b0
transcendence12 <gosiek.krawczuk@gmail.com>
2024-09-15 19:11:39 +0200

    style: title

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -3,10 +3,10 @@ import { FormComponent } from "../../../components/form";
 export default function Home() {
   return (
     <div>
-      <h1 className="text-5xl font-semibold m-auto text-center mt-12 text-slate-600">
+      <h1 className="text-5xl font-semibold m-auto text-center mt-12 text-slate-800">
         Pomocnik zakupowy AI
       </h1>
-      <h2 className="max-w-[800px] text-xl font-semibold m-auto text-center mt-12 text-slate-400">
+      <h2 className="max-w-[800px] text-xl font-semibold m-auto text-center mt-12 text-slate-600">
         Hej! Oto Twój pomocnik zakupowy, wprowadź nazwę pożądanego produktu i
         ciesz się najtańszą i najlepiej ocenianą ofertą na rynku!
       </h2>
